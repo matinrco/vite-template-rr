7ced58df424752077301dbd0e7adbc51ec6916f7
Matin Rezaei <matinrco@gmail.com>
2025-02-23 18:21:30 +0330

    Sort stylelint config
    * disable `selector-id-pattern` rule

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,11 +2,6 @@
 export default {
   extends: ["stylelint-config-standard-scss"],
   rules: {
-    "scss/no-duplicate-mixins": null,
-    "scss/at-mixin-pattern": null,
-    "scss/at-rule-no-unknown": null,
-    "custom-property-pattern": null,
-    "custom-property-empty-line-before": null,
     "declaration-empty-line-before": null,
     "declaration-block-no-redundant-longhand-properties": null,
     "alpha-value-notation": null,
@@ -17,7 +12,13 @@ export default {
     "comment-empty-line-before": null,
     "value-keyword-case": null,
     "media-feature-range-notation": null,
+    "custom-property-pattern": null,
+    "custom-property-empty-line-before": null,
+    "scss/no-duplicate-mixins": null,
+    "scss/at-mixin-pattern": null,
+    "scss/at-rule-no-unknown": null,
     "selector-class-pattern": null,
+    "selector-id-pattern": null,
     "selector-not-notation": null,
     "selector-pseudo-class-no-unknown": [
       true,
