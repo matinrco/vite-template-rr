864ba1ac3d7bd8313931bbf1e1765e9b8632f9b3
Matin Rezaei <matinrco@gmail.com>
2025-04-23 23:53:46 +0330

    Add axios interceptor placeholder

--- app/query/config/axios.ts
+++ app/query/config/axios.ts
@@ -4,4 +4,16 @@ export const axios = genericAxios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
+axios.interceptors.request.use((config) => {
+  // intercepted request is here
+
+  return config;
+});
+
+axios.interceptors.response.use((response) => {
+  // intercepted response is here
+
+  return response;
+});
+
 export type { AxiosResponse };
