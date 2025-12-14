293b9c3de86da608b4f70eeb98eb0ad2db8e9a1c
Matin Rezaei <matinrco@gmail.com>
2025-12-14 19:36:57 +0330

    Use mantine style in search params playground

--- app/routes/search-params-playground.tsx
+++ app/routes/search-params-playground.tsx
@@ -1,65 +1,68 @@
 import type { FC } from "react";
 import { useSearchParams } from "react-router";
+import { Button, Container, Group } from "@mantine/core";
 import type { Route } from "./+types/search-params-playground";
 
 const Component: FC<Route.ComponentProps> = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   return (
-    <div>
-      <button
-        onClick={() =>
-          setSearchParams(
-            {
-              firstName: "John",
-              lastName: "Doe",
-              products: ["p1", "p2", "p3"],
-            },
-            { replace: true },
-          )
-        }
-      >
-        add initial params and remove others
-      </button>
-      <button onClick={() => setSearchParams(undefined, { replace: true })}>
-        clear all params
-      </button>
-      <button
-        onClick={() => {
-          searchParams.set("id", "12345678");
-          setSearchParams(searchParams, { replace: true });
-        }}
-      >
-        add ID
-      </button>
-      <button
-        onClick={() => {
-          searchParams.set("status", "idle");
-          searchParams.set("username", "jdoe");
-          setSearchParams(searchParams, { replace: true });
-        }}
-      >
-        add status and username
-      </button>
-      <button
-        onClick={() => {
-          searchParams.delete("products", "p3");
-          setSearchParams(searchParams, { replace: true });
-        }}
-      >
-        delete 3rd product
-      </button>
-      <button
-        onClick={() => {
-          if (searchParams.has("status")) {
-            searchParams.set("status", "happy");
-            setSearchParams(searchParams, { replace: true });
+    <Container py="md">
+      <Group>
+        <Button
+          onClick={() =>
+            setSearchParams(
+              {
+                firstName: "John",
+                lastName: "Doe",
+                products: ["p1", "p2", "p3"],
+              },
+              { replace: true },
+            )
           }
-        }}
-      >
-        update status to happy
-      </button>
-    </div>
+        >
+          add initial params and remove others
+        </Button>
+        <Button onClick={() => setSearchParams(undefined, { replace: true })}>
+          clear all params
+        </Button>
+        <Button
+          onClick={() => {
+            searchParams.set("id", "12345678");
+            setSearchParams(searchParams, { replace: true });
+          }}
+        >
+          add ID
+        </Button>
+        <Button
+          onClick={() => {
+            searchParams.set("status", "idle");
+            searchParams.set("username", "jdoe");
+            setSearchParams(searchParams, { replace: true });
+          }}
+        >
+          add status and username
+        </Button>
+        <Button
+          onClick={() => {
+            searchParams.delete("products", "p3");
+            setSearchParams(searchParams, { replace: true });
+          }}
+        >
+          delete 3rd product
+        </Button>
+        <Button
+          onClick={() => {
+            if (searchParams.has("status")) {
+              searchParams.set("status", "happy");
+              setSearchParams(searchParams, { replace: true });
+            }
+          }}
+        >
+          update status to happy
+        </Button>
+      </Group>
+    </Container>
   );
 };
 
